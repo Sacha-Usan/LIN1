297fbc3df602b65c96a360060f6f346ff6832ef7
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2022-10-14 10:57:22 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo 'Oyez Oyez'
 
-HOSTNAME="SRV-LIN1-02"
+NEW_HOSTNAME="SRV-LIN1-02"
 
 #sudo echo "$HOSTNAME" > /etc/hostname
 #sudo bash -c 'echo "$HOSTNAME" > /etc/hostname'
-sudo bash -c 'echo "$HOSTNAME"'
+sudo bash -c 'echo "$NEW_HOSTNAME"'
 
 #sudo sed -i '2s/.*/127.0.1.1\ SRV-LIN1-02' etc/hosts
 sudo bash -c "sed -i '2s/.*/127.0.0.1\ $HOSTNAME/' /etc/hosts"
