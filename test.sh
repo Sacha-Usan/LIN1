c63bbe4e13f59dead128033f831ff6b1daa9ad10
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2022-10-14 10:56:38 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@ echo 'Oyez Oyez'
 HOSTNAME="SRV-LIN1-02"
 
 #sudo echo "$HOSTNAME" > /etc/hostname
-sudo bash -c 'echo "$HOSTNAME" > /etc/hostname'
+#sudo bash -c 'echo "$HOSTNAME" > /etc/hostname'
+sudo bash -c 'echo "$HOSTNAME"'
 
 #sudo sed -i '2s/.*/127.0.1.1\ SRV-LIN1-02' etc/hosts
 sudo bash -c "sed -i '2s/.*/127.0.0.1\ $HOSTNAME/' /etc/hosts"
