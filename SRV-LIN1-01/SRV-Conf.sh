4de805f70927d304ef492d41dd59d873737602f4
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2022-10-29 09:18:15 +0200

    Update SRV-Conf.sh

--- SRV-LIN1-01/SRV-Conf.sh
+++ SRV-LIN1-01/SRV-Conf.sh
@@ -2,15 +2,29 @@
 
 # Implémentation de la clef publique
 
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-touch ~/.ssh/authorized_keys
+mkdir /~/.ssh
+chmod 700 /~/.ssh
+touch /~/.ssh/authorized_keys
 
-cat << EOM > ~/.ssh/authorized_keys
+cat << EOM > /~/.ssh/authorized_keys
 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKtwmkAffyMfwgGnw+kgfgNHPmElZj9qMUlXPnQibizA/tzO3UEWapFiq1/0Rc6h2ixJFLvKeef5NnFo1AMDWNg= ecdsa-key-20221015
 EOM
 
 cat << EOM > /etc/ssh/sshd_config
+Include /etc/ssh/sshd_config.d/*.conf
+
 PasswordAuthentication yes
 PermitEmptyPasswords no
+
+ChallengeResponseAuthentication no
+
+UsePAM yes
+
+X11Forwarding yes
+
+PrintMotd no
+
+AcceptEnv LANG LC_*
+
+Subsystem       sftp    /usr/lib/openssh/sftp-server
 EOM
