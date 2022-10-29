075a4cc5f5bf0a93cd051c9c4d34e4679f2710a7
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2022-10-29 10:20:57 +0200

    Update SRV-Conf.sh

--- SRV-LIN1-01/SRV-Conf.sh
+++ SRV-LIN1-01/SRV-Conf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+####################################
 # Implémentation de la clef publique
 
 mkdir /home/cpnv/.ssh
@@ -27,4 +28,51 @@ PrintMotd no
 AcceptEnv LANG LC_*
 
 Subsystem       sftp    /usr/lib/openssh/sftp-server
+EOM
+
+#############################
+# Définition du nom de l'hôte
+
+hostnamectl set-hostname SRV-LIN1-01
+
+###########################
+# Définition de l'interface
+
+cat << EOM > /etc/network/interfaces
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto enp0s3
+iface enp0s3 inet static
+address 10.10.10.11
+netmask 255.255.255.0
+gateway 10.10.10.1
+EOM
+
+##########################
+# Définition fichier hosts
+
+cat << EOM > /etc/hosts
+127.0.0.1       localhost
+127.0.1.1       SRV-LIN1-01
+10.10.10.11     SRV-LIN1-01.lin1.local SRV-LIN1-01
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+EOM
+
+#########################################
+# Définition du fichier de résolution DNS
+
+cat << EOM > /etc/resolv.conf
+domain lin1.local
+search lin1.local
+nameserver 10.10.10.11
+nameserver 10.10.10.1
 EOM
