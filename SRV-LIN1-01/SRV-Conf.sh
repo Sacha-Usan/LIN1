c55e953175318b977209755292cd6bbe18adfd4c
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2022-10-29 09:30:05 +0200

    Update SRV-Conf.sh

--- SRV-LIN1-01/SRV-Conf.sh
+++ SRV-LIN1-01/SRV-Conf.sh
@@ -2,11 +2,11 @@
 
 # Implémentation de la clef publique
 
-mkdir /~/.ssh
-chmod 700 /~/.ssh
-touch /~/.ssh/authorized_keys
+mkdir /home/cpnv/.ssh
+chmod 700 /home/cpnv/.ssh
+touch /home/cpnv/.ssh/authorized_keys
 
-cat << EOM > /~/.ssh/authorized_keys
+cat << EOM > /home/cpnv/.ssh/authorized_keys
 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKtwmkAffyMfwgGnw+kgfgNHPmElZj9qMUlXPnQibizA/tzO3UEWapFiq1/0Rc6h2ixJFLvKeef5NnFo1AMDWNg= ecdsa-key-20221015
 EOM
 
