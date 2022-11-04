c05aaa0d84444eab816712827f6d9f683bd94c77
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2022-11-04 14:54:17 +0100

    Update OpenLDAP.sh

--- SRV-LIN1-01/OpenLDAP.sh
+++ SRV-LIN1-01/OpenLDAP.sh
@@ -15,6 +15,7 @@ apt install slapd ldap-utils -y
 cat << EOM > /etc/hosts
 127.0.0.1       localhost
 10.10.10.11     SRV-LIN1-01.lin1.local  SRV-LIN1-01 ldap
+
 # The following lines are desirable for IPv6 capable hosts
 ::1     localhost ip6-localhost ip6-loopback
 ff02::1 ip6-allnodes
@@ -24,7 +25,7 @@ EOM
 ########################
 # Configuration OpenLDAP
 
-dpkg-reconfigure slapd -y
+dpkg-reconfigure slapd
 
 # Omit OpenLDAP server configuration ? : Non
 # DNS domain name : lin1.local
