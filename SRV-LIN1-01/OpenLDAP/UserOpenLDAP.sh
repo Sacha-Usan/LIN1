af175bb6e36cac70c4d4c4a34a70ce62b178e14b
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2022-11-12 08:38:58 +0100

    Create UserOpenLDAP.sh

---
+++ SRV-LIN1-01/OpenLDAP/UserOpenLDAP.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+#####################################
+# Création répertoire et fichier ldif
+
+mkdir -p /etc/ldap/content
+
+chmod 755 /etc/ldap/content
+
+touch /etc/ldap/content/base.ldif /etc/ldap/content/groups.ldif /etc/ldap/content/users.ldif /etc/ldap/content/addtogroup.ldif
+
+##########################################
+# Configuration des groupes d'utilisateurs
+
+cat << EOM > /etc/ldap/content/base.ldif
+dn: ou=users,dc=lin1,dc=local
+objectClass: organizationalUnit
+objectClass: top
+ou: users
+
+dn: ou=groups,dc=lin1,dc=local
+objectClass: organizationalUnit
+objectClass: top
+ou: groups
+EOM
+
+ldapadd -x -D cn=admin,dc=lin1,dc=local -W -f /etc/ldap/content/base.ldif
+
+##########################################
+
+cat << EOM > /etc/ldap/content/groups.ldif
+dn: cn=Managers,ou=groups,dc=lin1,dc=local
+objectClass: top
+objectClass: posixGroup
+gidNumber: 20000
+
+dn: cn=Ingenieurs,ou=groups,dc=lin1,dc=local
+objectClass: top
+objectClass: posixGroup
+gidNumber: 20010
+
+dn: cn=Devloppeurs,ou=groups,dc=lin1,dc=local
+objectClass: top
+objectClass: posixGroup
+gidNumber: 20020
+EOM
+
+ldapadd -x -D cn=admin,dc=lin1,dc=local -W -f /etc/ldap/content/groups.ldif
+
+#########################################
+
+cat << EOM > /etc/ldap/content/users.ldif
+dn: uid=man1,ou=users,dc=lin1,dc=local
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: person
+uid: man1
+userPassword: Pa$$w0rd
+cn: Man 1
+givenName: Man
+sn: 1
+loginShell: /bin/bash
+uidNumber: 10000
+gidNumber: 20000
+displayName: Man 1
+homeDirectory: /home/man1
+mail: man1@lin1.local
+description: Man 1 account
+
+dn: uid=man2,ou=users,dc=lin1,dc=local
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: person
+uid: man2
+userPassword: Pa$$w0rd
+cn: Man 2
+givenName: Man
+sn: 2
+loginShell: /bin/bash
+uidNumber: 10001
+gidNumber: 20000
+displayName: Man 2
+homeDirectory: /home/man1
+mail: man2@lin1.local
+description: Man 2 account
+
+dn: uid=ing1,ou=users,dc=lin1,dc=local
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: person
+uid: ing1
+userPassword: Pa$$w0rd
+cn: Ing 1
+givenName: Ing
+sn: 1
+loginShell: /bin/bash
+uidNumber: 10010
+gidNumber: 20010
+displayName: Ing 1
+homeDirectory: /home/man1
+mail: ing1@lin1.local
+description: Ing 1 account
+
+dn: uid=ing2,ou=users,dc=lin1,dc=local
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: person
+uid: ing2
+userPassword: Pa$$w0rd
+cn: Ing 2
+givenName: Ing
+sn: 2
+loginShell: /bin/bash
+uidNumber: 10011
+gidNumber: 20010
+displayName: Ing 2
+homeDirectory: /home/man1
+mail: ing2@lin1.local
+description: Ing 2 account
+
+dn: uid=dev1,ou=users,dc=lin1,dc=local
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: person
+uid: dev1
+userPassword: Pa$$w0rd
+cn: Dev 1
+givenName: Dev
+sn: 1
+loginShell: /bin/bash
+uidNumber: 10020
+gidNumber: 20020
+displayName: Dev 1
+homeDirectory: /home/man1
+mail: dev1@lin1.local
+description: Dev 1 account
+EOM
+
+ldapadd -x -D cn=admin,dc=lin1,dc=local -W -f /etc/ldap/content/users.ldif
+
+##############################################
+
+cat << EOM > /etc/ldap/content/addtogroup.ldif
+dn: cn=Managers,ou=groups,dc=lin1,dc=local
+changetype: modify
+add: memberuid
+memberuid: man1
+
+dn: cn=Managers,ou=groups,dc=lin1,dc=local
+changetype: modify
+add: memberuid
+memberuid: man2
+
+dn: cn=Ingenieurs,ou=groups,dc=lin1,dc=local
+changetype: modify
+add: memberuid
+memberuid: ing1
+
+dn: cn=Ingenieurs,ou=groups,dc=lin1,dc=local
+changetype: modify
+add: memberuid
+memberuid: ing2
+
+dn: cn=Devloppeurs,ou=groups,dc=lin1,dc=local
+changetype: modify
+add: memberuid
+memberuid: dev1
+EOM
+
+ldapadd -x -D cn=admin,dc=lin1,dc=local -W -f /etc/ldap/content/addtogroup.ldif
